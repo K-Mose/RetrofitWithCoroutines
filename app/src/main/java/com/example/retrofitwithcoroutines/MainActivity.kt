78b4ada11261616090d0c44e9585e7a3b5c5bde2
user <km_413@naver.com>
2022-01-15 20:18:54 +0900

    View Data

--- app/src/main/java/com/example/retrofitwithcoroutines/MainActivity.kt
+++ app/src/main/java/com/example/retrofitwithcoroutines/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.retrofitwithcoroutines
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.lifecycle.liveData
+import com.example.retrofitwithcoroutines.databinding.ActivityMainBinding
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val retService = RetrofitInstance.getRetrofitInstance().create(AlbumService::class.java)
+        val responseLiveData: LiveData<Response<Albums>> = liveData {
+            val response = retService.getAlbums()
+            emit(response)
+        }
+        responseLiveData.observe(this, Observer {
+            val albumsList = it.body()?.listIterator()
+            if(albumsList != null) {
+                while (albumsList.hasNext()) {
+                    val albumsItem = albumsList.next()
+                    val text = "  " + "Album Title : ${albumsItem.title} \n"+
+                               "  " + "Album Id : ${albumsItem.id} \n"+
+                               "  " + "Album UserId : ${albumsItem.userId} \n\n\n"
+                    Log.i("TAG :: ", "$text")
+                    binding.tvText.append(text)
+                }
+            }
+        })
+
     }
 }
